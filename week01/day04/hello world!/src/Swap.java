1836faabfcc95c5cfefc62fb3d18e8ca1b966772
Norbert Szabad <sznorb14@gmail.com>
2019-01-17 16:31:10 +0100

    Swap

---
+++ week01/day04/hello world!/src/Swap.java
@@ -0,0 +1,21 @@
+public class Swap {
+    public static void main(String[] args){
+
+        int a, b, c;
+                a = 123;
+                b = 526;
+        System.out.println("Az a eredeti értéke: " + a);
+        System.out.println("A b eredeti értéke: " + b);
+
+            c = a;
+            a = b;
+            b = c;
+
+        System.out.println("Az a csrélt értéke: " + a);
+        System.out.println("A b cserélt értéke: " + b);
+
+
+
+    }
+
+}
