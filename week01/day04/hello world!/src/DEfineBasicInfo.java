0c78a55eacc7526b48bfb5a43d3f9dad7a765a35
Norbert Szabad <sznorb14@gmail.com>
2019-01-17 17:32:30 +0100

    DefineBasicInfo

---
+++ week01/day04/hello world!/src/DEfineBasicInfo.java
@@ -0,0 +1,19 @@
+public class DEfineBasicInfo {
+
+    public static void main(String[] args) {
+
+        String myName = "Norbert Szabad";
+        System.out.println("My name is " + myName);
+
+        int myAge = 34;
+        System.out.println("My age is " + myAge);
+
+        double myHeight = 1.83;
+        System.out.println("My height is " + myHeight +"m");
+
+        boolean marriageStatus = true;
+        System.out.println("I am not married: " + marriageStatus);
+
+    }
+
+    }
---
+++ week01/day04/hello world!/src/VariableMutation.java
@@ -0,0 +1,37 @@
+public class VariableMutation {
+    public static void main(String[] args) {
+
+        int a = 3;
+            a += 10;
+        System.out.println(a);
+
+        int b = 100;
+            b -= 7;
+        System.out.println(b);
+
+        int c = 44;
+            c *=2;
+        System.out.println(c);
+
+        int d =125;
+            d /=5;
+        System.out.println(d);
+
+        int  e = 8;
+        int cube = e * e * e;
+        System.out.println(cube);
+
+        int f1 = 123;
+        int f2 = 345;
+        boolean ifTrue = f1 > f2;
+        System.out.println(ifTrue);
+
+        int g1 = 350;
+        int g2 = 200;
+        boolean bigger = g1 < 2*g2;
+        System.out.println(bigger);
+
+        
+
+    }
+}
