e50d61990e09b7b20aade5e1fdab432cfc6ad8b7
Norbert Szabad <sznorb14@gmail.com>
2019-01-21 15:32:08 +0100

    SumElements

---
+++ week02/day01/src/Doubling.java
@@ -0,0 +1,25 @@
+public class Doubling {
+
+    public static void main (String [] args) {
+
+        int baseNum = 123;
+        doubling (baseNum);
+        System.out.println(baseNum);
+
+    }
+    public static int doubling (int num) {
+
+        num = num *2;
+
+        return num;
+
+    }
+
+
+}
+
+
+
+
+
+
---
+++ week02/day01/src/SumElements.java
@@ -0,0 +1,11 @@
+public class SumElements {
+
+    public static void main(String[] args) {
+        int r[] = {54, 23, 66, 12};
+
+        int sum = r[1] + r[2];
+
+        System.out.println("Sum of 2nd and 3rd number is: " + sum);
+
+    }
+}
