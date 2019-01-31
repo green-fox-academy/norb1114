9c2b22592821abbaad4840ea815f3a5193ccf79d
Norbert Szabad <sznorb14@gmail.com>
2019-01-31 14:41:13 +0100

    Bunny2

--- week03/day04/src/bunies2/Bunny2.java
+++ week03/day04/src/bunies2/Bunny2.java
@@ -2,6 +2,18 @@ package bunies2;
 
 public class Bunny2 {
     public static void main(String[] args) {
-        
+
+        System.out.println(bunny2ears(3));
     }
+        public static int bunny2ears(int bunnies){
+            if(bunnies <= 0){
+                return 0;
+            }else if (bunnies % 2 != 0) {
+                return bunny2ears(bunnies - 1) + 2;
+            }else {
+                return  bunny2ears(bunnies - 1) + 3;
+        }
+    }
+
 }
+
